b754dd8829fb32bac9b3c0aadccb5206f3730469
php110 <php110@gmail.com>
2018-08-16 16:34:44 +0800

    finish static pages

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+//构建静态页面
+Route::get('/', 'StaticPagesController@home');
+Route::get('/help', 'StaticPagesController@help');
+Route::get('/about', 'StaticPagesController@about');
